dd27d30382203e0591a8b79e87c24584f8e225e8
conner-replogle <95052372+conner-replogle@users.noreply.github.com>
2025-04-13 19:46:51 -0500

    Remove suggestion

--- Workflow/lib/suggested.ts
+++ Workflow/lib/suggested.ts
@@ -7,30 +7,30 @@ export function useSuggestedExercises(previous: Exercise[]){
   
     
    useEffect(() => {
-        let path = `http://localhost:8080/api/exercises/suggested?previous=${encodeURI(JSON.stringify(previous.map((a)=>a.id!)))}`
-        fetch(path, {
+        // let path = `http://localhost:8080/api/exercises/suggested?previous=${encodeURI(JSON.stringify(previous.map((a)=>a.id!)))}`
+        // fetch(path, {
           
-        })
-        .then((response) => {   
+        // })
+        // .then((response) => {   
             
-            if (!response.ok) {
-                console.log('Response status:', response.status);
-                console.log('Response status text:', response.statusText);
+        //     if (!response.ok) {
+        //         console.log('Response status:', response.status);
+        //         console.log('Response status text:', response.statusText);
 
-                throw new Error(`Network response was not ok ${path}`,);
-            }
-            return response.json();
-        }
-        )
-        .then((data) => {
-            console.log('Suggested exercises:', data);
-            setSuggestedExercises(data);
-        }
-        )
-        .catch((error) => {
-            console.error('Error fetching suggested exercises:', error);
-        }
-        );
+        //         throw new Error(`Network response was not ok ${path}`,);
+        //     }
+        //     return response.json();
+        // }
+        // )
+        // .then((data) => {
+        //     console.log('Suggested exercises:', data);
+        //     setSuggestedExercises(data);
+        // }
+        // )
+        // .catch((error) => {
+        //     console.error('Error fetching suggested exercises:', error);
+        // }
+        // );
     }, [ exercises, previous]);
     
     return suggestedExercises;
